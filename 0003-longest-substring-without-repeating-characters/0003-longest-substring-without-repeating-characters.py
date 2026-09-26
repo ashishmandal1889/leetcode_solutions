b3bd51d47cc87f014e91db6e25cfbd9eb100dab4
Ashish Kumar <ashishmandal1889@gmail.com>
2026-09-26 10:26:21 +0530

    Time: 358 ms (12.84%), Space: 19.9 MB (72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,7 +5,6 @@ class Solution:
         char_map = {}
         for high in range(len(s)):
             char_map[s[high]] = char_map.get(s[high],0)+1
-            k = high-low+1
             while len(char_map)<(high-low+1):
                 char_map[s[low]] -=1
                 if char_map[s[low]]==0:
